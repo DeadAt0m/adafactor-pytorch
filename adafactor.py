39837f38719a600e37b396d41c4dd8d57dec6379
deadat0m <kheldi@yandex.ru>
2018-12-16 14:48:44 +0300

    fixed dirty code in __init__, mnist_test notebook was refactored for new pytorch api

--- adafactor.py
+++ adafactor.py
@@ -3,48 +3,37 @@ import operator
 import functools
 from copy import copy
 from math import sqrt
-#lambda,cuda
+
 class AdaFactor(torch.optim.Optimizer):
     def __init__(self, params, lr=None, beta1=0.9, beta2=0.999, eps1=1e-30, 
                  eps2=1e-3, cliping_threshold=1,non_constant_decay = True,
-                 enable_factorization=True,  weight_decay=0):
-        enable_momentum =  False
-        ams_grad = True
+                 enable_factorization=True, ams_grad=True, weight_decay=0):
+        
+        enable_momentum =  beta1 != 0
         self.beta1_glob = copy(beta1)
         self.beta2_glob = copy(beta2)
         self.lr_glob = copy(lr)
         
-        if type(beta2) == 'function':
-            enable_momentum = True
-        elif beta1 != 0:
-            enable_momentum = True
-            if not 0.0 <= beta1 < 1.0:
-                raise ValueError("Invalid beta parameter at index 0: {}".format(beta1))
-            beta1 =lambda x: self.beta1_glob
-        
-        
-        
+        beta1 = self.beta1_glob if hasattr(beta1,'__call__') else lambda x: self.beta1_glob
+        beta2 = self.beta2_glob if hasattr(beta2,'__call__') else lambda x: self.beta2_glob
+
         if non_constant_decay:
             ams_grad = False
-            if not 0.0 <= beta2 < 1.0:
-                raise ValueError("Invalid beta parameter at index 1: {}".format(beta2))
-            beta1 = lambda t: self.beta1_glob * (1 - self.beta1_glob ** (t-1)) / (1 - self.beta1_glob ** t)
-            beta2 = lambda t: self.beta2_glob * (1 - self.beta2_glob ** (t-1)) / (1 - self.beta2_glob ** t)
-        elif type(beta2) != 'function':    
-            beta2=lambda x: self.beta2_glob
-        else:
-            ams_grad = False    
-                        
+            if isinstance(self.beta1_glob,float):
+                beta1 = lambda t: self.beta1_glob * (1 - self.beta1_glob ** (t-1)) / (1 - self.beta1_glob ** t)
+            if isinstance(self.beta2_glob,float):
+                beta2 = lambda t: self.beta2_glob * (1 - self.beta2_glob ** (t-1)) / (1 - self.beta2_glob ** t)
+
+        relative_step_size  = True
         
-        relative_step_size  = False
         if lr is None:
             #default value from article
             lr = lambda t: min(1e-2, 1 / sqrt(t))
-        elif type(lr) == 'function':
-            relative_step_size  = True
-            del self.lr_glob
-        else:
+            
+        if isinstance(self.lr_glob, float):
             lr=lambda x: self.lr_glob
+            relative_step_size = False
+  
                          
         defaults = dict(lr=lr, beta1=beta1, beta2=beta2, eps1=eps1,
                         eps2=eps2, cliping_threshold=cliping_threshold,                                                                                 weight_decay=weight_decay,ams_grad=ams_grad,
@@ -100,9 +89,9 @@ class AdaFactor(torch.optim.Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead') 
                     
-                flag1, flag2 = self._check_shape(grad.size())
+                is_matrix, is_need_reshape = self._check_shape(grad.size())
                 new_shape = p.data.size()
-                if flag2 and group['enable_factorization']:
+                if is_need_reshape and group['enable_factorization']:
                     new_shape, old_shape =\
                     self._experimental_reshape(p.data.size())
                     grad = grad.view(new_shape)
@@ -114,7 +103,7 @@ class AdaFactor(torch.optim.Optimizer):
                         state['exp_avg'] = torch.zeros(new_shape, dtype=torch.float32, device=p.grad.device)
                                            
                        
-                    if flag1 and group['enable_factorization']:
+                    if is_matrix and group['enable_factorization']:
                         state['exp_avg_sq_R'] = torch.zeros((1,new_shape[1]), dtype=torch.float32, device=p.grad.device)
                         state['exp_avg_sq_C'] = torch.zeros((new_shape[0],1), dtype=torch.float32, device=p.grad.device)
                     else:
@@ -126,7 +115,7 @@ class AdaFactor(torch.optim.Optimizer):
                 if group['enable_momentum']:
                     exp_avg = state['exp_avg']
                     
-                if flag1 and group['enable_factorization']:
+                if is_matrix and group['enable_factorization']:
                     exp_avg_sq_R = state['exp_avg_sq_R']
                     exp_avg_sq_C = state['exp_avg_sq_C'] 
                 else:
@@ -139,7 +128,7 @@ class AdaFactor(torch.optim.Optimizer):
                 state['step'] += 1
                 lr_t = group['lr'](state['step'])
                 if group['relative_step_size']:
-                    lr_t *= max(group['eps2'], self._rms(data))
+                    lr_t *= max(group['eps2'], self._rms(p.data))
                           
                 if group['enable_momentum']:
                     beta1_t = group['beta1'](state['step'])
@@ -147,7 +136,7 @@ class AdaFactor(torch.optim.Optimizer):
                     
                 beta2_t = group['beta2'](state['step']) 
 
-                if flag1 and group['enable_factorization']:
+                if is_matrix and group['enable_factorization']:
                     exp_avg_sq_R.mul_(beta2_t).add_(1 - beta2_t,                   
                       torch.sum(torch.mul(grad,grad).add_(group['eps1']), dim=0, keepdim=True))
                     exp_avg_sq_C.mul_(beta2_t).add_(1 - beta2_t,                   
@@ -170,7 +159,7 @@ class AdaFactor(torch.optim.Optimizer):
                     u = torch.div(g,v.sqrt()) 
                     
                 u.div_(max(1,self._rms(u) / group['cliping_threshold']))
-                p.data.add_(-lr_t * (u.view(old_shape) if flag2 and group['enable_factorization'] else u))
+                p.data.add_(-lr_t * (u.view(old_shape) if is_need_reshape and group['enable_factorization'] else u))
             
  
                 if group['weight_decay'] != 0:
