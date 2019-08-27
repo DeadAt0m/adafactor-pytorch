561e627239c29c0be11256171a795b49e0404098
DeadAt0m <kheldi@yandex.ru>
2019-08-27 18:11:58 +0300

    Update adafactor.py
    fix of previous fix,stupid I

--- adafactor.py
+++ adafactor.py
@@ -84,6 +84,7 @@ class AdaFactor(torch.optim.Optimizer):
                 if p.grad is None:
                     continue
                 grad = p.grad.data
+                data_backup = p.data.clone().detach()
 
                     
                 if grad.is_sparse:
@@ -157,12 +158,11 @@ class AdaFactor(torch.optim.Optimizer):
                     u = torch.div(g,(torch.div(v,1 - beta2_t ** state['step'])).sqrt().add_(group['eps1']))
                 else:
                     u = torch.div(g,v.sqrt()) 
-                
-                if group['weight_decay'] != 0:
-                    p.data.add_(-group['weight_decay'] * lr_t, p.data)
-                    
+       
                 u.div_(max(1,self._rms(u) / group['cliping_threshold']))
                 p.data.add_(-lr_t * (u.view(old_shape) if is_need_reshape and group['enable_factorization'] else u))
-  
+                
+                if group['weight_decay'] != 0:
+                    p.data.add_(-group['weight_decay'] * lr_t, data_backup)
                     
         return loss
